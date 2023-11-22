604730344566e13487318625699dc01317202805
ammar arief <ammararief321@gmail.com>
2023-11-22 07:06:52 +0700

    prisma gen

--- app/registered/page.tsx
+++ app/registered/page.tsx
@@ -36,7 +36,7 @@ const RegisteredPage = async () => {
       <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
         <div className="flex max-w-[980px] flex-col items-start gap-2">
           <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-            Thank you for registering for the Big1boxing competition
+            Thank you for registering for the Big1boxing competition.
           </h1>
         </div>
         <Table>
